88ec494481b04055259f1ecd87711dce7d4276de
Mohammad Zeinhom <zzzeinhom@gmail.com>
2026-07-02 06:19:01 +0300

    Remove unused Chrome driver imports
    Drop the unused `Service` and `ChromeDriverManager` imports from `list_expander.py`.

--- list_expander.py
+++ list_expander.py
@@ -1,8 +1,6 @@
 import time, json
 from urllib.parse import urlparse
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
